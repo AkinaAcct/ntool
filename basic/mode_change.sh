5a969b1453f69843d821d8694b52c2b0fd8d9be3
nya-sec <nyaisacat@gmail.com>
2023-05-13 11:36:03 +0800

    test

--- basic/mode_change.sh
+++ basic/mode_change.sh
@@ -1,15 +1,19 @@
 mode_change(){
     case $(cat ${MAINPATH}/.mode) in
-        0)
+        NORMAL_MULTI-FILE_MODE)
             cp -f ${NTOOLLIB}/ntsource ntsource.mode0
             cat ${NTOOLLIB}/basic/* >> ${NTOOLLIB}/ntsource
             cat ${NTOOLLIB}/container/* >> ${NTOOLLIB}/ntsource
-            echo -e "SINGLE_FILE_MODE" > ${MAINPATH}/.mode
+            echo "SINGLE_FILE_MODE" > ${MAINPATH}/.mode
             ;;
-        1)
+        SINGLE_FILE_MODE)
             rm ${NTOOLLIB}/ntsource
             mv ntsource.mode0 ntsource
             echo "NORMAL_MULTI-FILE_MODE" > ${MAINPATH}/.mode
+            ;;
+        *)
+            echo -e "${RED}出戳了!请重新安装ntool ;(${RESET}"
+            exit 1
             ;;
     esac
     echo -e "${GREEN}切换完成!${RESET}现在模式:$(cat ${MAINPATH}/.mode)${RESET}"
