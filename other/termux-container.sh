4df1014c10d886d8f2e97f1ddb4edfc6dc58cd58
nya-sec <nyaisacat@gmail.com>
2023-05-16 00:37:11 +0800

    new

---
+++ other/termux-container.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#from https://github.com/moe-hacker/termux-container
+
+install_termux-container() {
+    echo "please wait..."
+    pkg i -y make
+    git clone https://github.com/moe-hacker/termux-container ~/termux-container
+    cd ~/termux-container
+    make .
+    apt install -f ./termux-container.deb
+}
