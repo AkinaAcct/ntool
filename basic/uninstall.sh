7017ca2e2a7aff1a8b41ea607e05a160d14848a1
nya-sec <nyaisacat@gmail.com>
2023-05-17 23:06:37 +0800

    fix some bugs and something new

---
+++ basic/uninstall.sh
@@ -0,0 +1,21 @@
+function uninstall_ntool() {
+    echo -e "${RED}你的ntool数据将丢失!容器等将被完全删除!"
+    read -p "你确定要卸载ntool吗?  [Y|N]" CHOICE
+    echo -e "${RESET}"
+    case ${CHOICE} in
+        Y|y)
+            rm -rf ${NTOOLLIB} 
+            rm -rf ${MAINPATH}
+            echo -e "${GREEN}完成.ntool已经被卸载了.${RESET}"
+            exit 0
+            ;;
+        N|n)
+            termux_main_tui
+            ;;
+        *)
+            echo -e "${RED}未知的选择: \"${CHOICE}\" .exited.${RESET}"
+            exit 1
+            ;;
+    esac
+    exit 0
+}
