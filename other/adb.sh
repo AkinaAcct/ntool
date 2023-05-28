725f5b482e6f34491037663901fd1e1ffdefbce0
nya-sec <nyaisacat@gmail.com>
2023-05-28 09:54:18 +0800

    fix some bugs

--- other/adb.sh
+++ other/adb.sh
@@ -1,5 +1,7 @@
 #事实上,这个功能的初衷是我自己要用XD
-set -x
+
+LOCALIP="$(ifconfig | grep "inet" | grep -v "127.0.0.1" | grep -v "inet6" | awk '{print $2}' | tr -d "addr:")"
+
 function check_tun(){
     ifconfig | awk '{print $1}' | grep tun
     EXITSTATUS=$?
@@ -8,6 +10,18 @@ function check_tun(){
         read -p "按回车继续"
         adb_main
     fi
+}
+
+function adb_connect() {
+    echo -e "${GREEN}请进入手机的开发者选项打开无线adb功能,并选择使用配对码配对."
+    read -p "输入出现的端口: " PORT
+    adb connect ${LOCALIP}:${PORT}
+    EXITSTATUS=$?
+    if [ ${EXITSTATUS} != 0 ];then
+        echo -e "${RED}出错了!请重新运行!${RESET}"
+        exit 1
+    fi
+    adb devices -l
 }
 
 function adb_pair(){
@@ -20,21 +34,24 @@ function adb_pair(){
     check_tun
     echo -e "${RESET}"
     echo -e "在下方输入配对码"
-    adb pair $(ifconfig | grep "inet" | grep -v "127.0.0.1" | grep -v "inet6" | awk '{print $2}' | tr -d "addr:"):${PAIRPORT} ${PAIRCODE}
+    adb pair ${LOCALIP}:${PAIRPORT} ${PAIRCODE}
     EXITSTATUS=$?
-    read -p "end of pairing"
     if [ ${EXITSTATUS} != 0 ];then
         echo -e "${RED}配对出错了!如果你还是不知道如何配对,请参考${BLUE}shizuku${RED}的配对方法.${RESET}"
         exit 1
     fi
-    read -p "end of adb_pair"
+    touch .pair_success ${MAINPATH}
 }
 
 function adb_main(){
-    adb_pair
+    if [ ! -f "${MAINPATH}/.pair_success" ];then
+        adb_pair
+    fi
+    adb_connect
     CHOICE=$(dialog --output-fd 1 --title "ntool:adb_main" --menu "本页面所有功能仅供支持无线adb的设备使用!\n选择一个以继续" 15 70 8 \
     "1" "修复类原生ROM网络连接受限" \
-    "2" "进入rec/fastboot" \
+    "2" "修改安卓进程限制(保后台)" \
+    "3" "进入rec/fastboot" \
     "0" "返回上级菜单")
     case ${CHOICE} in
         1)
@@ -69,6 +86,16 @@ function adb_main(){
             read -p "你确定要继续吗?你将进入${MODE}模式!你未保存的工作数据将丢失! [CTRL-C:exit ENTER:continue]"
             echo -e "${RESET}"
             adb reboot ${MODE}
+            ;;
+        3)
+            MAXPROCNMUM=$(dialog --output-fd 1 --title "ntool:adb" --inputbox "输入要修改的最大进程数\n最大为21474836473" 15 70)
+            if [ -z ${MAXPROCNMUM} ];then
+                bad_empty_input
+                adb_main
+                exit 0
+            fi
+            adb shell device_config set_sync_disabled_for_tests persistent
+            adb shell device_config put activity_manager max_phantom_processes ${MAXPROCNMUM}
             ;;
         0)
             other_main_tui
