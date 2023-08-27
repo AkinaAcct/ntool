8dc00ec6ea7d718e46eb2d08fa8c5748ad66f33e
nya-sec <nyaisacat@gmail.com>
2023-08-27 21:27:31 +0800

    fix bugs on update script

--- basic/update.sh
+++ basic/update.sh
@@ -1,13 +1,13 @@
 check_and_update() {
     echo -e "${BLUE}正在拉取最新版本号...${RESET}"
-    if [ "$(curl -s ${RAWURL}/version)" != "$(cat ${MAINPATH}/local_version)" ]; then
-        if (dialog --title "ntool-tui:install&update" --yesno "你的版本不是最新版。是否更新？\n最新版:$(curl ${RAWURL}/version 2>/dev/null)\n本地版本:$(cat ${MAINPATH}/local_version)" 15 70); then
+    if [ "$(curl -s ${RAWURL}/version)" != "$(cat ${MAINPATH}/.local_version)" ]; then
+        if (dialog --title "ntool-tui:install&update" --yesno "你的版本不是最新版。是否更新？\n最新版:$(curl ${RAWURL}/version 2>/dev/null)\n本地版本:$(cat ${MAINPATH}/.local_version)" 15 70); then
             echo -e "${BLUE}更新中...${RESET}"
             pkg up -y
             pkg install -y git wget curl python x11-repo dialog jq
             rm -rf ${NTOOLLIB}
             git clone https://github.com/nya-main/ntool ${NTOOLLIB}
-            curl -s ${RAWURL}/version >${MAINPATH}/local_version
+            curl -s ${RAWURL}/version >${MAINPATH}/.local_version
             echo -e "${GREEN}完成!输入ntool以启动!${RESET}"
             exit 0
         else
