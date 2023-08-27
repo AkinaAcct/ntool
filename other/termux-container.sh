54d1683d34d3c4ccd1dc46aec6ddd878fb09f4b1
nya-sec <nyaisacat@gmail.com>
2023-08-27 17:24:21 +0800

    remove something.

--- other/termux-container.sh
+++ other/termux-container.sh
@@ -5,7 +5,7 @@ install_termux-container() {
     echo "please wait..."
     pkg i -y make
     git clone https://github.com/moe-hacker/termux-container ~/termux-container
-    cd ~/termux-container
+    cd ~/termux-container || exit 1
     make
     apt install -f ./termux-container.deb
 }
