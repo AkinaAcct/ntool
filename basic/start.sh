f381f2b2b6216809ba7102a59883e20c784ea87d
nya-sec <nyaisacat@gmail.com>
2023-08-27 21:50:03 +0800

    小毛病修复again and again

--- basic/start.sh
+++ basic/start.sh
@@ -22,9 +22,9 @@ start() {
     -v | --version)
         NTOOLVERSION="$(cat ${MAINPATH}/version 2>/dev/null)"
         if [ -z ${NTOOLVERSION} ]; then
-            echo -e "${RNTOOL}\n\tversion:${RED}ERR-FILE-NOT-FOUND${RESET}\n\tmode:${GREEN}normal mode${RESET}"
+            echo -e "${RNTOOL}\n\tversion:${RED}ERR-FILE-NOT-FOUND${RESET}"
         else
-            echo -e "${RNTOOL}\n\tversion:${BLUE}${NTOOLVERSION}${RESET}\n\tmode:${GREEN}$(cat ${NTOOLLIB}/.mode)${RESET}"
+            echo -e "${RNTOOL}\n\tversion:${BLUE}${NTOOLVERSION}${RESET}"
         fi
         exit 0
         ;;
